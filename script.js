0d3df8121077064ea7c8acc599af3639ef27d703
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-05-21 16:39:15 +0200

    Récupération des éléments html + création de la variable data

---
+++ script.js
@@ -0,0 +1,6 @@
+// Récupération des éléments HTML5
+const cardsGrid = document.querySelector(".cards__grid");
+const cards = document.querySelectorAll(".card");
+
+// Création de la variable data qui va stocker les données de l'API dans un tableau
+let data = [];
